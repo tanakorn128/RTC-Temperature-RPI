d6e3e12365bb104ed4ddf22529533c250091f6a9
thanakorn <60030041@kmitl.ac.th>
2020-10-23 14:21:14 +0100

    RTC DHT22

---
+++ tempDH22.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import sys
+import time
+import Adafruit_DHT
+sensor = Adafruit_DHT.DHT22
+pin = 8
+while True:
+  humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+  if humidity is not None and temperature is not None:
+    print 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity)
+    time.sleep(2)
+  else:
+    print 'Failed to get reading. Try again!'
