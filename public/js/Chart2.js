dc15e167e8b493bd440b273c7294ff9d521b50f4
Thanakorn <60030041@lmitl.ac.th>
2020-11-01 16:52:10 +0700

    1

--- public/js/Chart2.js
+++ public/js/Chart2.js
@@ -1,10 +1,13 @@
-var url = 'http://localhost:3010/getdata';
-//var url = 'http://raspberrypi.local:3010/getdata';
+//var url = 'http://localhost:3010/getdata';
+var url = 'http://raspberrypi.local:3010/getdata';
 
-const http = new XMLHttpRequest()
 
-http.open("GET", url)
-http.send()
-
-http.onload = () => console.log(http.responseText)
-
+var req = new XMLHttpRequest();
+req.responseType = 'json';
+req.open('GET', url, true);
+req.onload = function() {
+    var jsonResponse = req.response;
+    // do something with jsonResponse
+    console.log(jsonResponse);
+};
+req.send(null);
