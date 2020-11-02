30cb46349c025f9c71837e795a57bbe53406cba4
Thanakorn <60030041@lmitl.ac.th>
2020-11-02 17:08:57 +0700

    W

--- public/Chart.js
+++ public/Chart.js
@@ -1,9 +1,1 @@
 //let url = "http//raspberrypi.local:30010/getdata"
-
-function httpGet() {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", "http//raspberrypi.local:30010/getdata", false); // false for synchronous request
-    xmlHttp.send(null);
-    return xmlHttp.responseText;
-}
-console.log(httpGet());
